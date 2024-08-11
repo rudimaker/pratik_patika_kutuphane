25e3b9f9136fb1a2d59827a87e0d78b99d91bde4
pc <pc@DESKTOP-M7QJQNL>
2024-08-11 10:13:30 +0300

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratik_patika_kutuphane
+{
+    public class Book
+    {
+
+        // Ad, Yazar Adı, Yazar Soyadı, Sayfa Sayısı, Yayınevi, Kayit Tarihi bilgileri ile kitaplar kaydetmek istiyoruz.
+
+
+        public string KitapAd;
+        public string YazarAd;
+        public string YazarSoyad;
+        public int SayfaSayısı;
+        public string YayınEvi;
+        public DateTime KayıtTarihi;
+
+
+        // 2 Adet constructor alternatifi olsun.
+
+       // Bir tanesi default constructor tarzında parmetre almadan çalışan bir metot.
+        public Book()
+        {
+            KayıtTarihi = DateTime.Now;  // KayıtTarihi nesne oluşturulunca otomatik olarak o an olarak atansın. (Her iki constructor için de geçerli)
+        }
+
+        // Diğeri Kitap adı, Yazar Adı, Sayfa Sayısı, Yayınevi  bilgilerini alıp bu değerleri nesne üretim aşamasında atayan bir metot.
+        public Book(string kitapad,string yazarad, string yazarsoyad, int sayfasayısı, string yayınevi)
+        {
+            KitapAd = kitapad;
+            YazarAd = yazarad;
+            YazarSoyad = yazarsoyad;
+            SayfaSayısı = sayfasayısı;
+            YayınEvi = yayınevi;
+
+            KayıtTarihi = DateTime.Now; // KayıtTarihi nesne oluşturulunca otomatik olarak o an olarak atansın. (Her iki constructor için de geçerli)
+        }
+
+
+        public void DisplayList()
+        {
+            Console.WriteLine($"{KitapAd} | {YazarAd} {YazarSoyad} | {SayfaSayısı} | {YayınEvi} | {KayıtTarihi}");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using pratik_patika_kutuphane;
+
+Book kitap1 = new Book("Aylin", "Ayşe","Kulin",398,"Remzi Kitapevi");
+
+Book kitap2 = new Book();
+{
+    kitap2.KitapAd = "Fareler ve İnsanlar";
+    kitap2.YazarAd = "John";
+    kitap2.YazarSoyad = "Steinbeck";
+    kitap2.SayfaSayısı = 155;
+    kitap2.YayınEvi = "İletişim Yayınevi";
+}
+
+kitap1.DisplayList();
+kitap2.DisplayList();
